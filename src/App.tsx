9ae8078f9efd6352ad127a8c0056f405cf432144
mehmet ülker <mehmet-ulker@hotmail.com>
2025-10-30 21:53:53 +0300

    feat(app): render Products and Cart layout and fetch desserts from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import Cart from "./components/Cart";
+import Container from "./components/Container";
+import Products from "./components/Products";
+import CartEmpty from "./components/CartEmpty";
+import CartItems from "./components/CartItems";
+import ProductItem from "./components/ProductItem";
+import { type Dessert } from "./type/products";
+
 function App() {
+  const [products, setProduct] = useState<Dessert[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3000/desserts");
+      const data = await response.json();
+      setProduct(data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <h1>Hello word</h1>
-    </>
+    <main>
+      <Container>
+        <div className="grid grid-cols-[8fr_4fr] max-lg:grid-cols-1 gap-8 ">
+          <Products>
+            {products.map((product) => (
+              <ProductItem key={product.name} product={product} />
+            ))}
+          </Products>
+
+          <Cart>
+            {/* <CartEmpty /> */}
+            <CartItems />
+          </Cart>
+        </div>
+      </Container>
+    </main>
   );
 }
 
