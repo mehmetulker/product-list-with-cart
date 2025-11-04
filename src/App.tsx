a8817d750170cb89690e5512ee3794dc4d80ebc4
mehmet ülker <mehmet-ulker@hotmail.com>
2025-11-04 22:22:19 +0300

    fix(layout): add max-lg:mx-4 to grid container for proper spacing on small screens

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
   return (
     <main>
       <Container>
-        <div className="grid grid-cols-[8fr_4fr] max-lg:grid-cols-1 gap-8 ">
+        <div className="grid grid-cols-[8fr_4fr] max-lg:grid-cols-1  max-lg:mx-4 gap-8 ">
           <Products>
             {products.map((product) => (
               <ProductItem key={product.name} product={product} />
